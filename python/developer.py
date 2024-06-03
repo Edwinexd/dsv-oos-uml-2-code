f450d4cebf961fb1b279080e40de00337a5bd183
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-06-03 11:22:40 +0200

    Fix python developer implementation Didn't match java or the class diagram at all

--- python/developer.py
+++ python/developer.py
@@ -1,10 +1,10 @@
 from employee import Employee
 from academic_diploma import AcademicDiploma
 
-from typing import Iterable
+from typing import Iterable, List
 
 class Developer(Employee):
-    def __init__(self, name: str):
+    def __init__(self, name: str, diplomas: Iterable[AcademicDiploma]):
         super().__init__(name)
-        self._has: Iterable[AcademicDiploma] = []
+        self._earned: List[AcademicDiploma] = [diploma for diploma in diplomas]
 
